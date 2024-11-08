54088d8fdbeea2687d4c7e64d522f918bea77f7b
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2024-11-08 11:20:03 -0500

    Update staticram_shapiro.ino

--- staticram_shapiro.ino
+++ staticram_shapiro.ino
@@ -1,1 +1,62 @@
+// Arduino program to read data from CMOS Static RAM 48-pin BGA chip
 
+// Identify constant pins that will be set either high or low
+const int CE = 8;
+const int OE = 9; 
+const int CLK = 10;
+const int WE = 11;
+
+// Identify address pins
+
+// Identify I/O pins
+
+// Define time delays
+int delay_clock = 250;
+int ending_delay = 10;
+
+// Set pins as input or output and set constant pins to non-write levels
+void setup() {
+  pinMode(CS, OUTPUT);
+  pinMode(OE, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  pinMode(WE, OUTPUT);
+
+  digitalWrite(CS, HIGH);
+  digitalWrite(OE, HIGH);
+  digitalWrite(WE, LOW);
+}
+
+void loop() {
+  digitalWrite(CS, LOW);
+  digitalWrite(OE, LOW);
+  digitalWrite(WE, HIGH);
+
+for (int i = ?; i <= ?; i ++) {
+  send_address(0x??);
+}
+
+  digitalWrite(CS, HIGH);
+  digitalWrite(OE, HIGH);
+  digitalWrite(WE, LOW);
+
+  delay(ending_delay);
+}
+
+// Clock function
+void clock() {
+  digitalWrite(CLK, HIGH);
+  delayMicroseconds(delay_clock);
+  digitalWrite(CLK, LOW);
+  delayMicroseconds(delay_clock);
+  digitalWrite(CLK, HIGH);
+  delayMicroseconds(delay_clock);
+  digitalWrite(CLK, LOW);
+  delayMicroseconds(delay_clock);
+}
+
+void send_address(byte data, address_pin, output_pin) {
+  for (int i = 7; i >= 0; i --) {
+    digitalWrite(address_pin, (data >> i) & 0x01);
+    clock();
+  }
+}
